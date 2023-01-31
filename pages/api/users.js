f65ce7368e909ad8ea3a724617794d32a27b5e68
Snekxs <creepergang321@gmail.com>
2023-01-31 12:58:59 -0700

    Added More Stuff To User API

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,3 +1,26 @@
 export default function handler(req, res) {
-    res.status(200).json([{ name: "Snekxs" }, { name: "Wizdo" }, { "name": "TheOtherSnekxs" }, { "name": "TheOtherWizdo" }]);
+  res.status(200).json([
+    {
+      name: "Snekxs",
+      items: [{ mouse: "Ninjutso Sora" }, { mousepad: "Soku X1" }],
+    },
+    {
+      name: "Wizdo",
+      items: [{ mouse: "Fantech Aria" }, { mousepad: "Gamesense Radar 4mm" }],
+    },
+    {
+      name: "TheOtherSnekxs",
+      items: [
+        { mouse: "Pulsar Xlite V2 Mini" },
+        { mousepad: "TJ Exclusives Cerapad Kin" },
+      ],
+    },
+    {
+      name: "TheOtherWizdo",
+      items: [
+        { mouse: "Pulsar X2 Mini" },
+        { mousepad: "Aqua Control Plus+ Wave" },
+      ],
+    },
+  ]);
 }
