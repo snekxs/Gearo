c9a061f7c0523e398261c173a5f8007a3de24054
Snekxs <creepergang321@gmail.com>
2023-02-13 16:58:38 -0700

    Added Video Background

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,21 @@ import Header from "./components/header";
 import * as React from "react";
 import PostButton from "./components/postButton";
 
-const maintenance = false;
+const maintenance = true;
 
 const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   if (maintenance) {
     return (
       <>
+        <video
+          autoPlay
+          loop
+          muted
+          src="https://res.cloudinary.com/dzgm7ugq6/video/upload/v1676331065/background_online-video-cutter.com_xgaytd.mp4"
+          type="video/mp4"
+          className="backgroundVideo"
+        />
         <div className="maintenance">
           <h1>Be Right Back.</h1>
         </div>
