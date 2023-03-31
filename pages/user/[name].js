12f91de22925aa939ac99c3c54119ce852b41aeb
Snekxs <creepergang321@gmail.com>
2023-03-31 13:17:21 -0600

    fixed [user] request

--- pages/user/[name].js
+++ pages/user/[name].js
@@ -1,5 +1,5 @@
 import Header from "@/components/Header";
-import { ErrorRounded } from "@mui/icons-material";
+import { DataSaverOff, ErrorRounded } from "@mui/icons-material";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import ColorThief from "colorthief";
@@ -9,10 +9,10 @@ export default function UserPage(name) {
   const [data, setData] = useState();
   const [error, setError] = useState(false);
   const [imageData, setImageData] = useState(null);
-  const imgRef = useRef();
+  const userCardRef = useRef(null);
 
   useEffect(() => {
-    fetch(`/api/user/${name.name}`)
+    fetch(`/api/user/${name.name}`, { timeout: 10000 })
       .then((response) => {
         if (!response.ok) {
           throw new Error("User not found");
@@ -23,11 +23,36 @@ export default function UserPage(name) {
         setData(data);
       })
       .catch((error) => {
-        console.error(error);
-        setError(true);
-        // Handle the error appropriately (e.g. show an error message to the user)
+        if (error.name === "AbortError") {
+          console.log("Request timed out");
+        } else {
+          console.error(error);
+          setError(true);
+          // Handle other errors appropriately
+        }
       });
   }, []);
+
+  function createdDays(dateString) {
+    const diffTime = Math.abs(new Date() - new Date(dateString));
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    return diffDays === 0
+      ? "Created today"
+      : diffDays === 1
+      ? "Created yesterday"
+      : `Created ${diffDays} days ago`;
+  }
+
+  function status(status) {
+    if (status) {
+      return (
+        <div className="userStatus">
+          <h1>Online</h1>
+        </div>
+      );
+    }
+  }
 
   if (error) {
     return <h1>User not found</h1>;
@@ -36,13 +61,11 @@ export default function UserPage(name) {
       <div>
         <Header />
         {data?.map((user) => (
-          <div className="UserCard" key={user.id}>
+          <div className="UserCard" key={user.id} ref={userCardRef}>
             <img src={user.image_url} />
             <h1>{user.name}</h1>
-            <h1>Created Yesterday</h1>
-            <div className="userStatus">
-              <h1>Online</h1>
-            </div>
+            <h1 className="UserCreated">{createdDays(user.created_at)}</h1>
+            {status(user.online)}
           </div>
         ))}
       </div>
